040a1731de22797ce86674abe3122bc816048042
Karol Sygiet <karolsygiet@gmail.com>
2023-05-26 15:54:14 +0200

    [RTC-256] Add protobufs (#2)
    * remove debug method
    
    * add lint workflow
    
    * add workflow dispach for docs publishing
    
    * change swift version
    
    * Add protobufs
    
    * update readme
    
    * update readme

--- JellyfishClientSdk/README.md
+++ JellyfishClientSdk/README.md
@@ -1,3 +1,7 @@
 # JellyfishClientSdk
 
 A description of this package.
+
+# Description
+
+API documentation is available [here](https://jellyfish-dev.github.io/ios-client-sdk/documentation/jellyfishclientsdk/)
---
+++ compile_proto.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Terminate on errors
+set -e
+
+printf "Synchronising submodules... "
+git submodule sync --recursive >> /dev/null
+git submodule update --recursive --remote --init >> /dev/null
+printf "DONE\n\n"
+
+file="./protos/jellyfish/peer_notifications.proto"
+
+printf "Compiling: file $file\n"
+protoc --swift_out="./JellyfishClientSdk/Sources/JellyfishClientSdk" $file
+printf "DONE\n"
