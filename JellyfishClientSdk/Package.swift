98c7ef490bcc932d671f0fda386bcd8e4d70553d
skyman503 <karolsygiet@gmail.com>
2023-06-02 11:03:41 +0200

    added all recievers

--- JellyfishClientSdk/Package.swift
+++ JellyfishClientSdk/Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "JellyfishClientSdk",
     platforms: [
-            .iOS(.v13)
-        ],
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -15,9 +15,9 @@ let package = Package(
             targets: ["JellyfishClientSdk"])
     ],
     dependencies: [
-      .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
-      .package(url: "https://github.com/jellyfish-dev/membrane-webrtc-ios.git", branch:"graszka22/swift-package"),
-//      .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.0.0")
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(url: "https://github.com/jellyfish-dev/membrane-webrtc-ios.git", branch: "graszka22/swift-package"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,9 +25,9 @@ let package = Package(
         .target(
             name: "JellyfishClientSdk",
             dependencies: [
-              .product(name: "MembraneRTC", package: "membrane-webrtc-ios"),
-              .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-//              .product(name: "Starscream", package:"Starscream")
+                .product(name: "MembraneRTC", package: "membrane-webrtc-ios"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "Starscream", package: "Starscream"),
             ])
     ]
 )
--- JellyfishClientSdk/Sources/JellyfishClientSdk/JellyfishClientInternal.swift
+++ JellyfishClientSdk/Sources/JellyfishClientSdk/JellyfishClientInternal.swift
@@ -1,15 +1,23 @@
-import MembraneRTC
 import Foundation
+import MembraneRTC
+import Starscream
 
-internal class JellyfishClientInternal: JellyfishClientListener, MembraneRTCDelegate {
-    private var webSocket : URLSessionWebSocketTask?
+internal class JellyfishClientInternal: NSObject, JellyfishClientListener, MembraneRTCDelegate, WebSocketDelegate {
+    private var config: Config?
+    private var webSocket: WebSocket?
     var webrtcClient: MembraneRTC
 
     public init(delegate: MembraneRTCDelegate) {
-      self.webrtcClient = MembraneRTC.create(delegate: delegate)
+        self.webrtcClient = MembraneRTC.create(delegate: delegate)
     }
 
     func connect(config: Config) {
+        self.config = config
+        let url = URL(string: config.websocketUrl)
+
+        webSocket = WebSocket(url: url!)
+        webSocket?.delegate = self
+        webSocket?.connect()
     }
 
     func leave() {
@@ -18,94 +26,130 @@ internal class JellyfishClientInternal: JellyfishClientListener, MembraneRTCDele
 
     func cleanUp() {
         webrtcClient.disconnect()
-      webSocket?.cancel(with: .normalClosure, reason: nil)
+        webSocket?.disconnect()
         webSocket = nil
         onDisconnected()
     }
 
-  private func afterSent(error: Error?) -> Void {}
-  
-    private func sendEvent(peerMessage: Data) {
-        self.webSocket?.send(.data(peerMessage), completionHandler: afterSent)
-    }
+    func websocketDidConnect(socket: Starscream.WebSocketClient) {
+        onSocketOpen()
+        let authRequest = Jellyfish_PeerMessage.with({
+            $0.authRequest = Jellyfish_PeerMessage.AuthRequest.with({
+                $0.token = self.config?.token ?? ""
+            })
+        })
 
-    private func receiveEvent(event: SerializedMediaEvent) {
-      webrtcClient.receiveMediaEvent(mediaEvent: event)
-    }
-
-     func onJoinError(metadata: Any) {
-      print("xd")
-    }
-
-     func onJoinSuccess(peerID: String, peersInRoom: [Peer]) {
-    }
-
-     func onPeerJoined(peer: Peer) {
-    }
-
-     func onPeerLeft(peer: Peer) {
-    }
-
-     func onPeerUpdated(peer: Peer) {
-    }
-
-     func onSendMediaEvent(event: SerializedMediaEvent) {
-       let mediaEvent =
-         Jellyfish_PeerMessage.with({
-           $0.mediaEvent = Jellyfish_PeerMessage.MediaEvent.with({
-             $0.data = event
-           })
-         })
-       
-       guard let serialzedData = try? mediaEvent.serializedData() else {
-         return
-       }
+        guard let serialzedData = try? authRequest.serializedData() else {
+            return
+        }
         sendEvent(peerMessage: serialzedData)
     }
 
-     func onTrackAdded(ctx: TrackContext) {
+    func websocketDidDisconnect(socket: Starscream.WebSocketClient, error: Error?) {
+        onSocketClose(code: 1000, reason: "TODO")
     }
 
-     func onTrackReady(ctx: TrackContext) {
+    func websocketDidReceiveMessage(socket: Starscream.WebSocketClient, text: String) {
+        // UNSUPPORTED
+        onSocketError()
     }
 
-     func onTrackRemoved(ctx: TrackContext) {
+    func websocketDidReceiveData(socket: Starscream.WebSocketClient, data: Data) {
+        do {
+            let peerMessage = try Jellyfish_PeerMessage(serializedData: data)
+            if peerMessage.authenticated.isInitialized {
+                onAuthSuccess()
+            } else if peerMessage.mediaEvent.isInitialized {
+                receiveEvent(event: peerMessage.mediaEvent.data)
+            } else {
+                print("Received unexpected websocket message: $peerMessage")
+            }
+        } catch {
+            print("Unexpected error: \(error).")
+        }
     }
 
-     func onTrackUpdated(ctx: TrackContext) {
+    private func sendEvent(peerMessage: Data) {
+        self.webSocket?.write(data: peerMessage)
     }
 
-     func onRemoved(reason: String) {
+    private func receiveEvent(event: SerializedMediaEvent) {
+        webrtcClient.receiveMediaEvent(mediaEvent: event)
     }
 
-     func onBandwidthEstimationChanged(estimation: Int) {
+    func onJoinError(metadata: Any) {
+        print("xd")
     }
-  
+
+    func onJoinSuccess(peerID: String, peersInRoom: [Peer]) {
+    }
+
+    func onPeerJoined(peer: Peer) {
+    }
+
+    func onPeerLeft(peer: Peer) {
+    }
+
+    func onPeerUpdated(peer: Peer) {
+    }
+
+    func onSendMediaEvent(event: SerializedMediaEvent) {
+        let mediaEvent =
+            Jellyfish_PeerMessage.with({
+                $0.mediaEvent = Jellyfish_PeerMessage.MediaEvent.with({
+                    $0.data = event
+                })
+            })
+
+        guard let serialzedData = try? mediaEvent.serializedData() else {
+            return
+        }
+        sendEvent(peerMessage: serialzedData)
+    }
+
+    func onTrackAdded(ctx: TrackContext) {
+    }
+
+    func onTrackReady(ctx: TrackContext) {
+    }
+
+    func onTrackRemoved(ctx: TrackContext) {
+    }
+
+    func onTrackUpdated(ctx: TrackContext) {
+    }
+
+    func onRemoved(reason: String) {
+    }
+
+    func onBandwidthEstimationChanged(estimation: Int) {
+    }
+
     func onSocketClose(code: Int, reason: String) {
-      
+
     }
-  
+
     func onSocketError() {
-      
+
     }
-    
+
     func onSocketOpen() {
-      
+
     }
-    
+
     func onAuthSuccess() {
-      
+
     }
-    
+
     func onAuthError() {
-      
+
     }
-    
+
     func onDisconnected() {
-      
+
     }
-  
+
     func onTrackEncodingChanged(peerId: String, trackId: String, encoding: String) {
-      
+
     }
 }
